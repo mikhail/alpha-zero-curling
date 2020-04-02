e9eeee2d162f23b0d61ac2987cffe51e269ba377
Mikhail Simin <mikhail.simin@gmail.com>
2020-04-02 16:08:10 -0700

    Add generic TensorFlow NNet

--- tfwrap/NNet.py
+++ tfwrap/NNet.py
@@ -10,8 +10,8 @@ from utils import *
 from pytorch_classification.utils import Bar, AverageMeter
 from NeuralNet import NeuralNet
 
-import tensorflow as tf
-from .Connect4NNet import Connect4NNet as onnet
+import tensorflow.compat.v1 as tf
+from . import GameNNet
 
 args = dotdict({
     'lr': 0.001,
@@ -21,12 +21,9 @@ args = dotdict({
     'num_channels': 512,
 })
 
-
-## Code based on othello.NNetWrapper with minimal changes.
-
 class NNetWrapper(NeuralNet):
     def __init__(self, game):
-        self.nnet = onnet(game, args)
+        self.nnet = GameNNet.NNet(game, args)
         self.board_x, self.board_y = game.getBoardSize()
         self.action_size = game.getActionSize()
 
@@ -34,7 +31,7 @@ class NNetWrapper(NeuralNet):
         self.saver = None
         with tf.Session() as temp_sess:
             temp_sess.run(tf.global_variables_initializer())
-        self.sess.run(tf.variables_initializer(self.nnet.graph.get_collection('variables')))
+        # self.sess.run(tf.variables_initializer(self.nnet.graph.get_collection('variables')))
 
     def train(self, examples):
         """
@@ -42,18 +39,18 @@ class NNetWrapper(NeuralNet):
         """
 
         for epoch in range(args.epochs):
-            print('EPOCH ::: ' + str(epoch + 1))
+            print('EPOCH ::: ' + str(epoch+1))
             data_time = AverageMeter()
             batch_time = AverageMeter()
             pi_losses = AverageMeter()
             v_losses = AverageMeter()
             end = time.time()
 
-            bar = Bar('Training Net', max=int(len(examples) / args.batch_size))
+            bar = Bar('Training Net', max=int(len(examples)/args.batch_size))
             batch_idx = 0
 
             # self.sess.run(tf.local_variables_initializer())
-            while batch_idx < int(len(examples) / args.batch_size):
+            while batch_idx < int(len(examples)/args.batch_size):
                 sample_ids = np.random.randint(len(examples), size=args.batch_size)
                 boards, pis, vs = list(zip(*[examples[i] for i in sample_ids]))
 
@@ -77,7 +74,7 @@ class NNetWrapper(NeuralNet):
                 # plot progress
                 bar.suffix  = '({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss_pi: {lpi:.4f} | Loss_v: {lv:.3f}'.format(
                             batch=batch_idx,
-                            size=int(len(examples) / args.batch_size),
+                            size=int(len(examples)/args.batch_size),
                             data=data_time.avg,
                             bt=batch_time.avg,
                             total=bar.elapsed_td,
@@ -112,14 +109,14 @@ class NNetWrapper(NeuralNet):
             os.mkdir(folder)
         else:
             print("Checkpoint Directory exists! ")
-        if self.saver == None:
+        if self.saver == None:            
             self.saver = tf.train.Saver(self.nnet.graph.get_collection('variables'))
         with self.nnet.graph.as_default():
             self.saver.save(self.sess, filepath)
 
     def load_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
         filepath = os.path.join(folder, filename)
-        if not os.path.exists(filepath + '.meta'):
+        if not os.path.exists(filepath+'.meta'):
             raise("No model in path {}".format(filepath))
         with self.nnet.graph.as_default():
             self.saver = tf.train.Saver()
