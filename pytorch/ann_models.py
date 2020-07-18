df49902587abaa489454f6fa9ea6e2f0a2ac0edd
Mikhail Simin <mikhail.simin@gmail.com>
2020-07-17 17:47:30 -0700

    Add ANN model

---
+++ pytorch/ann_models.py
@@ -0,0 +1,45 @@
+import logging
+
+import torch
+import torch.nn.functional as F
+from torch import nn as nn
+
+from Game import Game
+
+
+class Model(nn.Sequential):
+    def __init__(self, game: Game, args):
+        self.args = args
+        logging.info(f"Creating ANN with {args.layers} layers 🧮")
+        self.board_x, self.board_y = game.getBoardSize()
+        in_features = self.board_y
+        height = 256
+        self.out_features = game.getActionSize()
+        hidden_layers = [torch.nn.ReLU() for _ in range(args.layers)]
+        super().__init__(
+            torch.nn.Linear(in_features, height),
+            *hidden_layers,
+            torch.nn.Linear(height, self.out_features),
+        )
+
+        logging.info(f"Created ANN model: %s", str(self))
+
+    def forward(self, s: torch.Tensor):
+        logging.debug(f's.size: {s.size()}')
+
+        s = super().forward(s)
+
+        logging.debug(f's.size: {s.size()}')  # [1, 34, 182]
+
+        s_flat = s.flatten()
+
+        size = s_flat.size()[0]
+        s_flat = s_flat.view(-1, size)
+        fc_pi = nn.Linear(size, self.out_features)
+        fc_v = nn.Linear(size, 1)
+
+        pi = fc_pi(s_flat)
+        v = fc_v(s_flat)
+        logging.debug('Returning %s %s', pi.size(), v.size())
+
+        return F.log_softmax(pi, dim=1), torch.tanh(v)
